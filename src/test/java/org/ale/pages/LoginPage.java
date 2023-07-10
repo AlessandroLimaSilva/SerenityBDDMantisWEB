3e51dcdd27dc1d7d0c127d6b1f2d13e992273d92
Alessandro <coderlima@gmail.com>
2023-07-10 00:10:20 -0300

    Fix: diversos

--- src/test/java/org/ale/pages/LoginPage.java
+++ src/test/java/org/ale/pages/LoginPage.java
@@ -44,7 +44,6 @@ public class LoginPage extends BasePage{
 
     public void acessaTelaDeLogin(){
         this.openNewTab();
-        //this.open();
         LOGGER.info("ACESSANDO A URL : "+GlobalParameters.URL_DEFAULT);
         driver.navigate().to(GlobalParameters.URL_DEFAULT);
         waitUntilPageReady();
